fce9fb74d478745d3bc3d6ff21a531f9eb372e82
marselsibgatullin <kzn145@yandex.ru>
2020-08-03 13:56:01 +0300

    practice 4

--- src/components/Learning/LearningContainer.jsx
+++ src/components/Learning/LearningContainer.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import Learning from "./Learning";
 import {connect} from "react-redux";
-import {setLinks} from "../../redux/learning-reducer";
-import { withRouter } from "react-router-dom";
+import {setLinksContent} from "../../redux/learning-reducer";
+import {withRouter} from "react-router-dom";
 
 class LearningContainer extends React.Component {
     componentDidMount() {
-        if(this.props.match.params.techsNames === "css")
-            this.props.setLinks(this.props.learningPage.techsLinks.css)
-        else if(this.props.match.params.techsNames === "html")
-            this.props.setLinks(this.props.learningPage.techsLinks.html)
-        else if(this.props.match.params.techsNames === "js")
-            this.props.setLinks(this.props.learningPage.techsLinks.js)
-        else if(this.props.match.params.techsNames === "php")
-            this.props.setLinks(this.props.learningPage.techsLinks.php)
-        else this.props.setLinks(this.props.learningPage.techsLinks.empty)
+        this.props.setLinksContent(this.props.match.params.techsNames, this.props.learningPage.techsLinks);
     }
-
 
     render() {
         return <Learning
@@ -28,11 +19,11 @@ class LearningContainer extends React.Component {
 
 let mapStateToProps = (state) => {
     return {
-        learningPage : state.learningPage
+        learningPage: state.learningPage
     }
 }
 
 let url = withRouter(LearningContainer);
 
-export default connect(mapStateToProps,{setLinks})(url);
+export default connect(mapStateToProps, {setLinksContent})(url);
 
